ac4ad128de4505a46eecd37eef3b05a6e1e694e6
Benjamin <bysy@users.noreply.github.com>
2015-09-27 21:45:47 -0500

    feat: Load images with Picasso instead of AsyncTask
    resolves #2

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -21,10 +21,10 @@ import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.squareup.picasso.Picasso;
 
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.BookService;
-import it.jaschke.alexandria.services.DownloadImage;
 
 
 public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -215,7 +215,8 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
-            new DownloadImage((ImageView) rootView.findViewById(R.id.bookCover)).execute(imgUrl);
+            final ImageView coverView = (ImageView) rootView.findViewById(R.id.bookCover);
+            Picasso.with(getContext()).load(imgUrl).into(coverView);
             rootView.findViewById(R.id.bookCover).setVisibility(View.VISIBLE);
         }
 
--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -18,9 +18,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.BookService;
-import it.jaschke.alexandria.services.DownloadImage;
 
 
 public class BookDetail extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -122,8 +123,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
-            new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
-            rootView.findViewById(R.id.fullBookCover).setVisibility(View.VISIBLE);
+            final ImageView coverView = (ImageView) rootView.findViewById(R.id.fullBookCover);
+            Picasso.with(getContext()).load(imgUrl).into(coverView);
+            coverView.setVisibility(View.VISIBLE);
         }
 
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
