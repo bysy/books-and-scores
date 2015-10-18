510c58ac27679304c52bfcfea5c25519cd98da51
Benjamin <bysy@users.noreply.github.com>
2015-10-18 18:24:08 -0500

    fix: Update summary widget as well after sync

--- football-scores/app/src/main/java/barqsoft/footballscores/widget/OverviewWidgetProvider.java
+++ football-scores/app/src/main/java/barqsoft/footballscores/widget/OverviewWidgetProvider.java
@@ -4,6 +4,9 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import barqsoft.footballscores.sync.SyncAdapter;
 
 /**
  * Created by Benjamin on 10/11/15.
@@ -12,6 +15,20 @@ public class OverviewWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+        update(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(SyncAdapter.ACTION_DATA_UPDATED)) {
+            update(context);
+        } else {
+            super.onReceive(context, intent);
+        }
+    }
+
+    private void update(Context context) {
+        Log.d(OverviewWidgetProvider.class.getSimpleName(), "updating summary widget");
         context.startService(new Intent(context, OverviewWidgetUpdaterService.class));
     }
 }
